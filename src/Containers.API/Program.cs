a0735a33ddd6ea31edbf32cc30ee20dd1e2c17e0
AntonSolianyk <s30252@pjwstk.edu.pl>
2025-04-18 11:49:21 +0200

    update

--- src/Containers.API/Program.cs
+++ src/Containers.API/Program.cs
@@ -1,9 +1,15 @@
+
+
+using Containers.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("UniversityDatabase");
+builder.Services.AddSingleton<IContainerService, ContainerService>(containerService => new ContainerService(connectionString));
 
 var app = builder.Build();
 
@@ -36,9 +42,24 @@ app.MapGet("/weatherforecast", () =>
     .WithName("GetWeatherForecast")
     .WithOpenApi();
 
+app.MapGet("/api/containers", (IContainerService containerService) =>
+{
+    try
+    {
+        return Results.Ok(containerService.GetAllContainers());
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
 app.Run();
+
+// app.MapPost()
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
+
--- src/Containers.Application/ContainerService.cs
+++ src/Containers.Application/ContainerService.cs
@@ -1,6 +1,69 @@
-﻿namespace Containers.Application;
+﻿// using System.ComponentModel;
+// using Containers.Models;
 
-public class ContainerService
+using Containers.Application;
+using Containers.Models;
+using Microsoft.Data.SqlClient;
+// namespace Containers.Application;
+
+public class ContainerService : IContainerService
 {
-    
+    public bool AddContainer(Container container)
+    {
+        const string insertString =
+            "INSERT INTO Containers(ContainerTypeId, IsHasazrdious, Name) VALUES (@ContainerTypeId, @IsHasazrdious, @Name)";
+        int countRowsAdded = -1;
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            // connection = command.ExecuteNonQuery();
+            SqlCommand command = new SqlCommand();
+            command.Parameters.AddWithValue("@ContainerTypeId", container.ContainerTypeId);
+            command.Parameters.AddWithValue("@IsHazardious", container.IsHazardous);
+            command.Parameters.AddWithValue("@Name", container.Name);
+            connection.Open();
+            countRowsAdded = command.ExecuteNonQuery();
+            
+        }
+        return countRowsAdded != -1;
+    }
+
+    private string _connectionString;
+
+    public ContainerService(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IEnumerable<Container> GetAllContainers()
+    {
+        List<Container> containers = [];
+        const string queryString = "SELECT * FROM Containers";
+        using (SqlConnection connection = new SqlConnection(_connectionString))
+        {
+            SqlCommand command = new SqlCommand(queryString, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            try
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        var containerRow = new Container
+                        {
+                            ID = reader.GetInt32(0),
+                            ContainerTypeId = reader.GetInt32(1),
+                            IsHazardous = reader.GetBoolean(2),
+                            Name = reader.GetString(3),
+                        };
+                    }
+                }
+            }
+            finally
+            {
+                reader.Close();
+            }
+        }
+        return containers;
+    }
 }
